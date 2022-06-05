76f0ae3c18b21a4b9123e83a3b8f43f2557d8030
teterina_m_o <teterina_m_o@rambler.ru>
2022-06-05 22:05:35 +0300

    fixed: schedule models

---
+++ Back/TutorProject.Account.Common/Models/Day.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TutorProject.Account.Common.Models
+{
+    public class Day
+    {
+        public Day()
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                DaySchedule.Add(false);
+            }
+        }
+        
+        public Guid Id { get; init; }
+
+        public DayOfWeek DayOfWeek { get; set; }
+
+        public List<bool> DaySchedule { get; init; } = new List<bool>();
+    }
+}
