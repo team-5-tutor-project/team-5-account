1789be24e251307d28dc9e81d9f52ef047fc6132
teterina_m_o <teterina_m_o@rambler.ru>
2022-06-06 11:34:54 +0300

    fixed Schedule

--- Back/TutorProject.Account.Common/Models/Schedule.cs
+++ Back/TutorProject.Account.Common/Models/Schedule.cs
@@ -5,19 +5,10 @@ namespace TutorProject.Account.Common.Models
 {
     public class Schedule
     {
-        public Schedule()
-        {
-            FreeTimeSchedule = new List<Day>();
-            for (int i = 0; i < 7; i++)
-            {
-                FreeTimeSchedule.Add(new Day());
-                
-            } 
-        }
         public Guid Id { get; init; }
         
         public Tutor Tutor { get; init; }
 
-        public List<Day> FreeTimeSchedule { get; init; }
+        public List<Day> FreeTimeSchedule { get; init; } = new List<Day>();
     }
 }
