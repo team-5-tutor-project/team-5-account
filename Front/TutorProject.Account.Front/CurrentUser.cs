10b92e3b2f7323304a35658074138d0b93ad908d
Kroexov <kroexov@gmail.com>
2022-06-12 23:40:55 +0300

    ACC-15
    добавлено отображение и ссылка на чаты, добавлен редирект на страницу с авторизацией если юзер не авторизован

--- Front/TutorProject.Account.Front/CurrentUser.cs
+++ Front/TutorProject.Account.Front/CurrentUser.cs
@@ -7,15 +7,24 @@ public static class CurrentUser
 {
     private static User _user;
 
+    private static bool _isAuthtorized;
+
     public static User User => _user ?? throw new AuthenticationException("Пользователь не авторизован");
 
     public static void Login(User user)
     {
         _user = user;
+        _isAuthtorized = true;
     }
 
     public static void Logout()
     {
         _user = null;
+        _isAuthtorized = true;
+    }
+
+    public static bool IsAuthorized
+    {
+        get => _isAuthtorized;
     }
 }
