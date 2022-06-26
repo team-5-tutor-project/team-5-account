b0a322cc09016f2cfce2120eee6e2a1d63e33d30
Mikhail <misha.rukovishnikow@yandex.ru>
2022-06-26 16:29:06 +0300

    ACC-6 добавлены куки

--- Front/TutorProject.Account.Front/Program.cs
+++ Front/TutorProject.Account.Front/Program.cs
@@ -1,11 +1,16 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TutorProject.Account.Front;
+using TutorProject.Account.Front.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:4000") });
 builder.Services.AddTransient<HttpService>();
+builder.Services.AddTransient<AuthorizationService>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
 
 await builder.Build().RunAsync();
---
+++ Front/TutorProject.Account.Front/Services/AuthorizationData.cs
@@ -0,0 +1,7 @@
+﻿namespace TutorProject.Account.Front.Services;
+
+public class AuthorizationData
+{
+    public string Token { get; set; }
+    public string UserType { get; set; }
+}
